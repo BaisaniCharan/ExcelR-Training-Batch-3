9c30f27fce799021ee5883160c0a62083cadab0b
BaisaniCharan <charan506106@gmail.com>
2025-01-27 11:03:34 +0530

    Greatest among 3

--- Demo003.java
+++ Demo003.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo3 {
+public class Demo003 {
 
     public static int greatestOfThree(int num1, int num2, int num3) {
         
@@ -25,13 +25,11 @@ public class demo3 {
 
         System.out.print("Enter the third number: ");
         int num3 = scanner.nextInt(); 
-
-      
+        
         int result = greatestOfThree(num1, num2, num3);
 
         System.out.println("The greatest of the three numbers is: " + result);
 
-    
         scanner.close();
     }
 }
