0b17a1c684ae4ed2b4a63b04895dac0451cf9b04
BaisaniCharan <charan506106@gmail.com>
2025-02-04 21:08:23 +0530

    FibonacciSeries

--- Demo020.java
+++ Demo020.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-public class Demo20 {
+public class Demo020 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user for the number of terms
         System.out.print("Enter the number of terms for the Fibonacci series: ");
         int terms = scanner.nextInt();
 
-        // Validate input
         if (terms <= 0) {
             System.out.println("Please enter a positive integer.");
         } else {
@@ -19,8 +17,6 @@ public class Demo20 {
 
         scanner.close();
     }
-
-    // Method to generate and print the Fibonacci series
     public static void generateFibonacci(int terms) {
         int first = 0, second = 1;
 
