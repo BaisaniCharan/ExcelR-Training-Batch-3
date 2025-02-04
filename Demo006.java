dce9f0a32758a946ebce10f0ad35a90e007e831b
BaisaniCharan <charan506106@gmail.com>
2025-02-04 11:38:31 +0530

    DO while

--- Demo006.java
+++ Demo006.java
@@ -6,18 +6,17 @@ public class Demo006 {
         String username;
         String password;
 
-        // Use a do-while loop to validate credentials
+        
         do {
             System.out.print("Enter username: ");
-            username = scanner.nextLine().trim(); // Trim to remove extra spaces
+            username = scanner.nextLine().trim(); 
             System.out.print("Enter password: ");
-            password = scanner.nextLine().trim(); // Trim to remove extra spaces
+            password = scanner.nextLine().trim(); 
 
             if (!(username.equals("Mahesh") && password.equals("Mahesh@123"))) {
                 System.out.println(+username"Welcome to Mallareddy University.\n");
             }
-        } while (!(username.equals("Mahesh") && password.equals("Mahesh@123"))); // Loop if credentials are incorrect
-
+        } while (!(username.equals("charan") && password.equals("charan@123"))); 
         // Success message
         System.out.println(username + ", Welcome to MRU!");
         scanner.close();
