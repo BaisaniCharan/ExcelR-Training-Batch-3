dde721f1fd34d4022e5bc3314b0a8bf3b4806ab8
BaisaniCharan <charan506106@gmail.com>
2025-02-10 10:36:33 +0530

    Fibonacci Series

--- Demo019.java
+++ Demo019.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Demo029{
+class Demo019{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of digits: ");
