4e8d7a8a7f37b3b8e8f66a54b4fe56fa5fc0cd40
BaisaniCharan <charan506106@gmail.com>
2025-02-10 12:45:35 +0530

    Area of triangle

--- Demo013.java
+++ Demo013.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
-class TriangleAreaDemo1 {
-
-	double getTriangleArea(int s1,int s2,int s3){
-		double s = (s1 + s2 + s3) / 2;
-		double ta = Math.sqrt(s * (s - s1) * (s - s2) * (s-s3));
-		return ta;
-	}
-	
+class Area {
+    double getTriangleArea(int s1, int s2, int s3) {
+        double s = (s1 + s2 + s3) / 2.0;
+        return Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
+    }
 }
+
 public class Demo013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter sides of the triangle: ");
-		int s1 = sc.nextInt();
+        int s1 = sc.nextInt();
         int s2 = sc.nextInt();
-        int s3 = sc.nextInt();		
-        TriangleAreaDemo1 ta = new TriangleAreaDemo1();
+        int s3 = sc.nextInt();
         
-		
-		System.out.println("The triangle area is : " + ta.getTriangleArea(s1, s2, s3));
-	}
+        Area ta = new Area();
+        System.out.println("The triangle area is: " + ta.getTriangleArea(s1, s2, s3));
+        
+        sc.close();
+    }
 }
