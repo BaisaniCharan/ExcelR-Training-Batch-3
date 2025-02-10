d111ee6584fcb2569db23aa3c9a7ab5c1b7cb508
BaisaniCharan <charan506106@gmail.com>
2025-02-10 10:25:17 +0530

    If we give a 2 digit nmber it give sum of 2 digits

--- Demo017.java
+++ Demo017.java
@@ -1,17 +1,13 @@
-class SumOf2Digits {
-	public static int getSumOf2Digits(int n){
-		int fd = n / 10;
-		int ld = n % 10;
-		int sum = fd + ld;
-		return sum;
-	}
-}
+import java.util.Scanner;
 
 public class Demo017 {
     public static void main(String[] args) {
-        SumOf2Digits obj = new SumOf2Digits();
-		int n = 78;
-		int sum = obj.getSumOf2Digits(n);
-		System.out.println("The sum of 2 digits is : "+sum);
-	}
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a two-digit number: ");
+        int number = sc.nextInt();
+        int tens = number / 10;
+        int ones = number % 10;
+        int sum = tens + ones;
+        System.out.println("The sum of the digits is: " + sum);
+    }
 }
